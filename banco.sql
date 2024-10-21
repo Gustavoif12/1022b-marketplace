66a7cf722771b1ceca48e727409cdcb4858d5aac
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2024-10-21 19:09:38 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,4 +1,4 @@
-USE defaultdb;
+/*USE defaultdb;
 
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -13,14 +13,42 @@ INSERT INTO produtos VALUES
 
 CREATE TABLE usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 INSERT INTO usuarios VALUES
-('Vinicius Menezes', 'vinicius.menezes@estudante.ifms.edu.br', '2024-10-21 10:30:00', '2024-10-21 10:30:00');
+(1, 'Vinicius Menezes', 'vinicius.menezes@estudante.ifms.edu.br', '2024-10-21 10:30:00', '2024-10-21 10:30:00');
+*/
+
+DROP DATABASE IF EXISTS banco1022b;
+CREATE DATABASE banco1022b;
+
+USE banco1022b;
+
+CREATE TABLE IF NOT EXISTS produtos(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    descricao VARCHAR(50),
+    preco DECIMAL(10,2),
+    imagem VARCHAR(300)
+);
+
+INSERT INTO produtos VALUES
+(1, "Iphone", "Celular RUIM", 5000.50, "SEM IMAGEM");
+
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+INSERT INTO usuarios VALUES
+(1, 'Vinicius Menezes', 'vinicius.menezes@estudante.ifms.edu.br', '2024-10-21 10:30:00', '2024-10-21 10:30:00');
 
 /*
 Criamos a tabela `usuarios` seguindo um modelo similar
