bd76ee479f88a91dd369b624e70ba7ee33940923
Gustavo Silva <118355959+Gustavoif12@users.noreply.github.com>
2024-10-28 08:19:09 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,25 @@ app.use(express.json())  //Middleware
 //incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
-app.get("/produtos", async (req, res) => {
-    //O que eu tenho que fazer aqui dentro?
-    //OK -> PASSO 1: Criar o banco de dados
-    //PASSO 2: Usar a lib mysql2 para conectar com o banco
-    try {
+app.post("/produtos",async(req,res)=>{
+    try{
         const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022b",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        //PASSO 3: QUERY  -> SELECT * FROM produtos
-        const [result, fields] = await conexao.query("SELECT * FROM produtos")
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                [id,nome,descricao,preco,imagem])
         await conexao.end()
-        //PASSO 4: Colocar os dados do banco no response
         res.send(result)
-    } catch (e) {
+    }catch(e){
+        console.log(e)
         res.status(500).send("Erro do servidor")
-    }
+    }  
 })
 
 // ROTA PARA LISTAR USUÁRIOS
