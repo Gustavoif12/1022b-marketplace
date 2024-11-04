47edbc314e07cadf281fcc54a8fbfb961c6201d5
Gustavo Silva <118355959+Gustavoif12@users.noreply.github.com>
2024-11-04 07:43:10 -0400

    commit

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,28 @@ app.use(express.json())  //Middleware
 //incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
+app.get("/produtos",async(req,res)=>{
+    //O que eu tenho que fazer aqui dentro?
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        //PASSO 3: QUERY  -> SELECT * FROM produtos
+        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
+        await conexao.end()
+        //PASSO 4: Colocar os dados do banco no response
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
 app.post("/produtos",async(req,res)=>{
     try{
         const conexao = await mysql.createConnection({
@@ -34,6 +56,7 @@ app.post("/produtos",async(req,res)=>{
     }  
 })
 
+/*
 // ROTA PARA LISTAR USUÁRIOS
 app.get("/usuarios", async (req, res) => {
     try {
@@ -53,8 +76,9 @@ app.get("/usuarios", async (req, res) => {
         res.status(500).send("Erro do servidor")
     }
 })
+*/
 
 //INICIAR O SERVIDOR
-app.listen(8000, () => {
+app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })
