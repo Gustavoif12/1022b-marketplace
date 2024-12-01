7a0897dae640f41ae6755230f5ab86daab270ea0
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2024-11-30 20:52:40 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,12 +3,12 @@ USE defaultdb;
 CREATE TABLE IF NOT EXISTS comics (
   id BIGINT PRIMARY KEY AUTO_INCREMENT,
   titulo VARCHAR(50),
-  autor VARCHAR(50),
+  autor VARCHAR(200),
   ano_de_publicacao INT,
   editora VARCHAR(50),
-  sinopse VARCHAR(200),
+  sinopse VARCHAR(500),
   preco DECIMAL(10, 2),
-  imagem VARCHAR(300)
+  imagem VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS reservas (
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS reservas (
 );
 
 INSERT INTO comics VALUES
-(1, 'Batman: Veneno', "Denny O'Neil, Russell Braun e Trevor Von Eeden", 2025, 'DC Panini Comics', 'Depois que o Homem-Morcego fracassa ao tentar salvar uma jovem, ele acaba se tornando viciado na perigosa substância conhecida como Veneno, e sua vida inteira sai totalmente de controle!', 69.90, 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_5h18p4fuu55ajfbchqt49tjb3a/-S897-FWEBP');
+(1, 'Batman: Veneno', "Denny O'Neil, Russell Braun e Trevor Von Eeden", 2025, 'DC Panini Comics', 'Depois que o Homem-Morcego fracassa ao tentar salvar uma jovem, ele acaba se tornando viciado na perigosa substância conhecida como Veneno, e sua vida inteira sai totalmente de controle', 69.90, 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_5h18p4fuu55ajfbchqt49tjb3a/-S897-FWEBP');
 
 INSERT INTO reservas VALUES
 (1, 'Gustavo Silva', 'Batman: Veneno', 'Cartão de crédito', '2024-11-25');
