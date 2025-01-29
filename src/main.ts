ccb73227e985cf933c46d622c141f54effd27aea
Gustavo Silva <gustavo.silva20@estudante.ifms.edu.br>
2025-01-29 10:08:17 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql2/promise';
+import BancoMysql from './db/bancoMysql';
 
 const app = express();
 
@@ -8,23 +9,18 @@ app.use(express.json());
 app.use(cors());
 
 // ROTA PARA CADASTRAR UMA COMIC
-app.post('/comics', async (req, res) => {
+app.post("/comics", async (req, res) => {
     try {
         const { id, titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem } = req.body;
+        console.log(id, titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem);
 
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "defaultdb",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
+        const banco = new BancoMysql();
+        const comic = { id: parseInt(id), titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem };
 
-        const query = `
-      INSERT INTO comics VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-        const [result] = await conexao.query(query, [id, titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem]);
-        await conexao.end();
+        const result = await banco.inserirComic(comic);
+        console.log(result);
+
+        await banco.end();
 
         res.status(201).send({ message: 'Comic cadastrada com sucesso!' });
     } catch (e) {
@@ -34,45 +30,81 @@ app.post('/comics', async (req, res) => {
 });
 
 // ROTA PARA LISTAR TODAS AS COMICS
-app.get('/comics', async (req, res) => {
+app.get("/comics", async (req, res) => {
     try {
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "defaultdb",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
+        const banco = new BancoMysql();
+        const result = await banco.listarComic();
+        console.log(result);
 
-        const [result] = await conexao.query('SELECT * FROM comics');
-        await conexao.end();
+        await banco.end();
         res.send(result);
     } catch (e) {
         console.error(e);
-        res.status(500).send('Erro do servidor');
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA LISTAR UMA COMIC PELO ID
+app.get("/comics/:id", async (req, res) => {
+    try {
+        const banco = new BancoMysql();
+        const result = await banco.listarComicPorId(req.params.id);
+        console.log(result);
+
+        await banco.end();
+        res.send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA ATUALIZAR UMA COMIC
+app.put("/comics/:id", async (req, res) => {
+    console.log("Tentando alterar comic de id:", req.params.id);
+    try {
+        const { titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem } = req.body;
+        const comic = { titulo, autor, ano_de_publicacao, editora, sinopse, preco, imagem };
+
+        const banco = new BancoMysql();
+        const result = await banco.alterarComic(req.params.id, comic);
+
+        res.status(200).send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA DELETAR UMA COMIC
+app.delete("/comics/:id", async (req, res) => {
+    console.log("Tentando excluir comic de id:", req.params.id);
+    try {
+        const banco = new BancoMysql();
+        const result = await banco.excluirComic(req.params.id);
+
+        res.status(200).send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
     }
 });
 
 // ROTA PARA CADASTRAR UMA RESERVA
-app.post('/reservas', async (req, res) => {
+app.post("/reservas", async (req, res) => {
     try {
         const { id, nome_comprador, titulo_comic, forma_pagamento, data_reserva } = req.body;
+        console.log(id, nome_comprador, titulo_comic, forma_pagamento, data_reserva);
 
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "defaultdb",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
+        const banco = new BancoMysql();
+        const reserva = { id: parseInt(id), nome_comprador, titulo_comic, forma_pagamento, data_reserva };
 
-        const query = `
-        INSERT INTO reservas VALUES (?, ?, ?, ?, ?)
-      `;
-        const [result] = await conexao.query(query, [id, nome_comprador, titulo_comic, forma_pagamento, data_reserva]);
-        await conexao.end();
+        const result = await banco.inserirReserva(reserva);
+        console.log(result);
 
-        res.status(201).send({ message: 'Reserva feita com sucesso!' });
+        await banco.end();
+
+        res.status(201).send({ message: 'Reserva cadastrada com sucesso!' });
     } catch (e) {
         console.error(e);
         res.status(500).send('Erro do servidor');
@@ -80,22 +112,63 @@ app.post('/reservas', async (req, res) => {
 });
 
 // ROTA PARA LISTAR TODAS AS RESERVAS
-app.get('/reservas', async (req, res) => {
+app.get("/reservas", async (req, res) => {
     try {
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "defaultdb",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        });
+        const banco = new BancoMysql();
+        const result = await banco.listarReserva();
+        console.log(result);
 
-        const [result] = await conexao.query('SELECT * FROM reservas');
-        await conexao.end();
+        await banco.end();
         res.send(result);
     } catch (e) {
         console.error(e);
-        res.status(500).send('Erro do servidor');
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA LISTAR UMA RESERVA PELO ID
+app.get("/reservas/:id", async (req, res) => {
+    try {
+        const banco = new BancoMysql();
+        const result = await banco.listarReservaPorId(req.params.id);
+        console.log(result);
+
+        await banco.end();
+        res.send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA ATUALIZAR UMA RESERVA
+app.put("/reservas/:id", async (req, res) => {
+    console.log("Tentando alterar reserva de id:", req.params.id);
+    try {
+        const { nome_comprador, titulo_comic, forma_pagamento, data_reserva } = req.body;
+        const reserva = { nome_comprador, titulo_comic, forma_pagamento, data_reserva };
+
+        const banco = new BancoMysql();
+        const result = await banco.alterarReserva(req.params.id, reserva);
+
+        res.status(200).send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+// ROTA PARA DELETAR UMA RESERVA
+app.delete("/reservas/:id", async (req, res) => {
+    console.log("Tentando excluir reserva de id:", req.params.id);
+    try {
+        const banco = new BancoMysql();
+        const result = await banco.excluirReserva(req.params.id);
+
+        res.status(200).send(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erro do servidor");
     }
 });
 
